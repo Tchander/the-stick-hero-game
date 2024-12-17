63d438ac0c4979856b3e88663b33a0e214e54e99
Evgeniy Chander <evgeniy.chander@maximumtest.ru>
2024-12-17 14:54:23 +0300

    Created The Stick Hero Game with js

---
+++ js/consts/index.js
@@ -0,0 +1,13 @@
+export const GamePhase = {
+  WAITING: 'waiting',
+  STRETCHING: 'stretching',
+  TURNING: 'turning',
+  WALKING: 'walking',
+  TRANSITIONING: 'transitioning',
+  FALLING: 'falling',
+};
+
+export const MouseEvent = {
+  UP: 'mouseup',
+  DOWN: 'mousedown',
+};
